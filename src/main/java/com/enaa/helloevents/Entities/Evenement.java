06be11f133ea3679ece5ac902e73887e09496151
hdfaouz <hdfaouzia2@gmail.com>
2025-05-20 23:31:23 +0100

    Creation des Entities

--- src/main/java/com/enaa/helloevents/Entities/Evenement.java
+++ src/main/java/com/enaa/helloevents/Entities/Evenement.java
@@ -1,7 +1,81 @@
 package com.enaa.helloevents.Entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 public class Evenement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String titre;
+    private String description;
+    private  String Location;
+    private Date date;
+    private double price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    @OneToMany(mappedBy = "reservation")
+    private Reservation reservation;
 
 
 }
--- src/main/java/com/enaa/helloevents/Entities/Reservation.java
+++ src/main/java/com/enaa/helloevents/Entities/Reservation.java
@@ -1,8 +1,39 @@
 package com.enaa.helloevents.Entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-
+import java.util.Date;
 
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date reservationDate;
+    private String statut;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(Date reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
 }
